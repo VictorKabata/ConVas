b45fb221e5bdae7bc13958b9db7e040aaf885d6a
Victor Kabata Ndung'u <39780120+VictorKabata@users.noreply.github.com>
2020-03-07 01:36:24 -0800

    Added MainActivity and editted navigation.xml

--- app/src/main/java/com/vickikbt/convas/SplashScreenActivity.java
+++ app/src/main/java/com/vickikbt/convas/SplashScreenActivity.java
@@ -1,8 +1,9 @@
 package com.vickikbt.convas;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+
+
     }
 }
